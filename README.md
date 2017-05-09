4596ae6c2b337667ce50324235607a69979397d0
Frédéric DROUET <fdrouet@exoplatform.com>
2017-05-09 22:58:35 +0200

    Convert README from asciidoctor to markdown for docker hub compatibility. (+1 squashed commit) Squashed commits: [00dc869] JMXTrans 265

---
+++ README.md
@@ -0,0 +1,77 @@
+# JMXTrans docker container for eXo
+
+This container aims to provide an out of the box tool to collect eXo Platform JMX metrics and to push it in an Influxdb server.
+
+# eXo Platform Docker image
+
+[![Docker Stars](https://img.shields.io/docker/stars/exoplatform/jmxtrans.svg)]() - [![Docker Pulls](https://img.shields.io/docker/pulls/exoplatform/jmxtrans.svg)]()
+
+|    Image                          |  JMXTrans  |   eXo Platform    
+|-----------------------------------|------------|--------------------
+| exoplatform/jmxtrans:latest       |   265      |   4.4+
+| exoplatform/jmxtrans:develop      |   265      |   4.4+
+
+# Running
+
+Just launch the image with default values :
+
+```
+docker run exoplatform/jmxtrans 
+```
+
+If you want to specify the targeted Influxdb server :
+
+```
+docker run -e TARGET_INFLUXDB_URL="http://influxdb.server.org" exoplatform/jmxtrans 
+```
+
+or the eXo Platform server hostname :
+
+```
+docker run -e TARGET_JMX_HOST=exo.server.org exoplatform/jmxtrans 
+```
+
+For more configuration settings, see the next section.
+
+# Configuration options
+
+Several aspects of JMXTrans container are customizable with the following environment variables:
+
+|    VARIABLE              |  MANDATORY  |   DEFAULT VALUE          |  DESCRIPTION
+|--------------------------|-------------|--------------------------|----------------
+| HEAP_SIZE | NO | `512` | specify the jvm allocated memory size in MB (-Xms and -Xmx parameters)
+| TARGET_JMX_HOST | NO | `localhost` | the JMX hostname of the eXo Platform instance
+| TARGET_JMX_PORT | NO | `8004` | the JMX port of the eXo Platform instance
+| TARGET_HOSTNAME | NO | `same as $TARGET_JMX_HOST value` | the hostname of the eXo Platform server
+| TARGET_NODE_ID | NO | `NC` | a string to identify an eXo node in a cluster for exemple
+| TARGET_INFLUXDB_URL | NO | `http://localhost:8086` | the full url of the Influxdb server to send the metrics
+| TARGET_INFLUXDB_DATABASE | NO | `exo` | the Influxdb database name to use
+| TARGET_INFLUXDB_USERNAME | NO | `nobody` | the Influxdb username
+| TARGET_INFLUXDB_PASSWORD | NO | `nothing` | the Influxdb password
+| TARGET_INFLUXDB_CREATE_DB | NO | `true` | does JMXTrans create the Influxdb database if needed
+| TARGET_INFLUXDB_RETENTION_POLICY | NO | `autogen` | the Influxdb rentention policy name to use
+| JMXTRANS_POOLING_FREQUENCY | NO | `30` | the JMXTrans pooling frequency in seconds
+| JMXTRANS_LOG_LEVEL | NO | `warn` | the JMXTrans logging level (debug\|info\|warn\|error\|fatal)
+
+# Collected metrics
+
+| Influxdb Measurement | MBeans 
+|----------------------|--------
+| jvm_gc | java.lang:type=GarbageCollector,name=* 
+| jvm_memory_heap | java.lang:type=Memory 
+| jvm_memory_pool | java.lang:name=*,type=MemoryPool 
+| jvm_system | java.lang:type=OperatingSystem 
+| jvm_threads| java.lang:type=Threading 
+| tomcat_datasources | Catalina:type=DataSource,class=javax.sql.DataSource,name=\"*\"
+| tomcat_request_processor | Catalina:type=GlobalRequestProcessor,name=*
+| tomcat_http_sessions | Catalina:type=Manager,context=/*,host=*
+| tomcat_threadpools | Catalina:type=ThreadPool,name=*
+
+
+# Testing
+
+A docker-compose file is provided to test a full monitoring stack with eXo Platform Community edition
+
+```
+docker-compose -f test/docker-compose.yml -p jmx up -d
+```
---
+++ jmxtrans.sh
@@ -0,0 +1,65 @@
+#!/bin/sh -u
+
+######################################
+## Configuration
+######################################
+HEAP_SIZE=${HEAP_SIZE:-"512"}
+## JMX Configuration (for remote system)
+TARGET_JMX_HOST=${TARGET_JMX_HOST:-"localhost"}
+TARGET_JMX_PORT=${TARGET_JMX_PORT:-"8004"}
+TARGET_HOSTNAME=${TARGET_HOSTNAME:-$TARGET_JMX_HOST}
+TARGET_NODE_ID=${TARGET_NODE_ID:-"NC"}
+## Influxdb Configuration
+TARGET_INFLUXDB_URL=${TARGET_INFLUXDB_URL:-"http://localhost:8086"}
+TARGET_INFLUXDB_DATABASE=${TARGET_INFLUXDB_DATABASE:-"exo"}
+TARGET_INFLUXDB_USERNAME=${TARGET_INFLUXDB_USERNAME:-"nobody"}
+TARGET_INFLUXDB_PASSWORD=${TARGET_INFLUXDB_PASSWORD:-"nothing"}
+TARGET_INFLUXDB_CREATE_DB=${TARGET_INFLUXDB_CREATE_DB:-"true"}
+TARGET_INFLUXDB_RETENTION_POLICY=${TARGET_INFLUXDB_RETENTION_POLICY:-"autogen"}
+# JMXTrans Configuration
+JMXTRANS_POOLING_FREQUENCY=${JMXTRANS_POOLING_FREQUENCY:-"30"} # the frequency is in seconds
+JMXTRANS_LOG_LEVEL=${JMXTRANS_LOG_LEVEL:-"warn"} # [debug|info|warn|error|fatal] (default: info)
+
+######################################
+## /!\ DON'T CHANGE ANYTHING BELOW /!\
+######################################
+LOG_LEVEL=${JMXTRANS_LOG_LEVEL}
+
+JMXTRANS_OPTS="${JMXTRANS_OPTS:-} -Duser.language=en -Duser.region=EN" # Always use the same Locale (en_EN) is a good practice to avoid localization problems between different servers
+JMXTRANS_OPTS="${JMXTRANS_OPTS} -DTARGET_JMX_HOST=${TARGET_JMX_HOST} -DTARGET_JMX_PORT=${TARGET_JMX_PORT}"
+JMXTRANS_OPTS="${JMXTRANS_OPTS} -DTARGET_INFLUXDB_URL=${TARGET_INFLUXDB_URL} -DTARGET_INFLUXDB_DATABASE=${TARGET_INFLUXDB_DATABASE}"
+JMXTRANS_OPTS="${JMXTRANS_OPTS} -DTARGET_INFLUXDB_USERNAME=${TARGET_INFLUXDB_USERNAME} -DTARGET_INFLUXDB_PASSWORD=${TARGET_INFLUXDB_PASSWORD}"
+JMXTRANS_OPTS="${JMXTRANS_OPTS} -DTARGET_INFLUXDB_CREATE_DB=${TARGET_INFLUXDB_CREATE_DB} -DTARGET_INFLUXDB_RETENTION_POLICY=${TARGET_INFLUXDB_RETENTION_POLICY}"
+JMXTRANS_OPTS="${JMXTRANS_OPTS} -DTARGET_HOSTNAME=${TARGET_HOSTNAME} -DTARGET_NODE_ID=${TARGET_NODE_ID}"
+JMXTRANS_OPTS="${JMXTRANS_OPTS} -Dlog4j.configuration=file://${JMXTRANS_APP_DIR}/log4j.xml"
+
+SECONDS_BETWEEN_RUNS=${JMXTRANS_POOLING_FREQUENCY}
+CONTINUE_ON_ERROR=false
+
+EXEC="-jar ${JMXTRANS_APP_DIR}/${JMXTRANS_JAR_FILE} -e -j ${JMXTRANS_JSON_DIR} -s ${SECONDS_BETWEEN_RUNS} -c ${CONTINUE_ON_ERROR} ${ADDITIONAL_JARS_OPTS:-}"
+GC_OPTS="-Xms${HEAP_SIZE}m -Xmx${HEAP_SIZE}m"
+#MONITOR_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false \
+#              -Dcom.sun.management.jmxremote.authenticate=false \
+#              -Dcom.sun.management.jmxremote.port=9999 \
+#              -Dcom.sun.management.jmxremote.rmi.port=9999 \
+#              -Djava.rmi.server.hostname=${TARGET_JMX_HOST}"
+
+if [ "$1" != "bash" -a "$1" != "sh" ]; then
+    echo "$1"
+    echo "$@"
+    echo "TARGET_JMX_HOST                   : ${TARGET_JMX_HOST}"
+    echo "TARGET_JMX_PORT                   : ${TARGET_JMX_PORT}"
+    echo "TARGET_HOSTNAME                   : ${TARGET_HOSTNAME}"
+    echo "TARGET_NODE_ID                    : ${TARGET_NODE_ID}"
+    echo "TARGET_INFLUXDB_URL               : ${TARGET_INFLUXDB_URL}"
+    echo "TARGET_INFLUXDB_DATABASE          : ${TARGET_INFLUXDB_DATABASE}"
+    echo "TARGET_INFLUXDB_USERNAME          : ${TARGET_INFLUXDB_USERNAME}"
+    echo "TARGET_INFLUXDB_CREATE_DB         : ${TARGET_INFLUXDB_CREATE_DB}"
+    echo "TARGET_INFLUXDB_RETENTION_POLICY  : ${TARGET_INFLUXDB_RETENTION_POLICY}"
+    echo "SECONDS_BETWEEN_RUNS              : ${SECONDS_BETWEEN_RUNS}"
+
+    gosu nobody:nogroup java -server ${JAVA_OPTS:-} ${JMXTRANS_OPTS} ${GC_OPTS} ${MONITOR_OPTS:-} ${EXEC}
+else
+    "$@"
+fi
+
